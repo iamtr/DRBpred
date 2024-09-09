6e9b9d8fa93c2834dd33646d74d253e04e8fe8cb
Jet <jettzeong@gmail.com>
2024-09-09 21:36:30 +0800

    Initial Commit

---
+++ temp.sh
@@ -0,0 +1,2 @@
+cp /home/tzejet/drb_jet/SPOT-1D-Single/results/*.csv /home/tzejet/drb_jet/output_features
+find /home/tzejet/drb_jet/DisPredict2_PSEE/DisPredict_v2.0/Software/Features -type f -name "*.57pfeatures" -exec cp {} "/home/tzejet/drb_jet/output_features" \;
